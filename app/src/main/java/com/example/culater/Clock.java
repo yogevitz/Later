7463221241d369fcbd0c0221864ab7838807eaa8
dor6688 <dor6688@gmail.com>
2019-03-27 22:17:40 +0200

    clock class: 1. counter 2. show points 3. back to menu
    menu class :
    1. when enter to this page it check if you inside with your coordinates in university
    2. button that get color : green if inside, otherwise red
    3. button to clock class

--- app/src/main/java/com/example/culater/Clock.java
+++ app/src/main/java/com/example/culater/Clock.java
@@ -1,5 +1,7 @@
 package com.example.culater;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.TextView;
+
+import java.util.List;
+import java.util.Properties;
 
 public class Clock extends AppCompatActivity {
 
@@ -21,8 +26,47 @@ public class Clock extends AppCompatActivity {
 
         chronometer = findViewById(R.id.chronometer);
         score = findViewById(R.id.score_TextView);
-        chronometer.start();
+        Thread t = new Thread(()->startClock());
+        t.start();
 
+
+        for(int i=0 ;i <10;i++)
+            System.out.println("Dor");
+        //int x = idle();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private void startClock() {
+        chronometer.start();
+        for(int i=0;i<10;i++) {
+            System.out.println("ITAY");
+        }
+
+    }
+
+    private int idle (){
+        ActivityManager am =(ActivityManager)this.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        ActivityManager.RunningTaskInfo task = tasks.get(0); // current task
+        ComponentName rootActivity = task.baseActivity;
+
+        String currentPackageName = rootActivity.getPackageName();
+        if (!currentPackageName.equals("com.example.culater")) {
+            System.out.println("Out");
+        }
+        return 2;
     }
 
     /**
@@ -35,6 +79,7 @@ public class Clock extends AppCompatActivity {
         System.out.println(elapsedMillis);
         point = elapsedMillis/30;
         score.setText("Point : "+point);
+
 
     }
 
